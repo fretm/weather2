65cbb26d380dbb2c47f7090af26d1e65a08a17b7
fre_frecho <frehiwottm@gmail.com>
2019-06-11 23:07:26 -0400

    min.js

---
+++ min.js
@@ -0,0 +1,22 @@
+function minimum(x,y)
+
+    {
+  
+ if (x<y)
+
+     {
+ return x +  " minimum"
+  }
+ 
+
+ else if (y<x)
+  
+ 
+       {
+   return  y+  "  minimum"
+
+}
+}
+
+
+console.log(minimum(677,88));
